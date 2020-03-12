41a2e937c3cd8fd7495e525238a55a9debc14456
poke-yoke <25385580+poke-yoke@users.noreply.github.com>
2020-03-12 09:47:21 +0000

    Update snmp.ps1
    Check for SNMP Services and install if required

--- snmp.ps1
+++ snmp.ps1
@@ -1,1 +1,11 @@
+#Read only community string
+$community_string = "bla"
+
+#Check If SNMP Services Are Already Installed
+$check = Get-WindowsFeature | Where-Object {$_.Name -eq "SNMP-Services"}
+If ($check.Installed -ne "True") {
+        #Install/Enable SNMP Services
+        Add-WindowsFeature SNMP-Services | Out-Null
+}
+
 
