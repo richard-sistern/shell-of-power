6a32a2e02faf3721f7fcbe35931bd72a6083223d
poke-yoke <25385580+poke-yoke@users.noreply.github.com>
2020-03-12 12:20:49 +0000

    Update snmp.ps1
    Added ability to set permitted managers

--- snmp.ps1
+++ snmp.ps1
@@ -1,5 +1,6 @@
 #Read only community string
 $community_string = "bla"
+$permitted_managers = @("bla", "bla")
 
 #Check If SNMP Services Are Already Installed
 $check = Get-WindowsFeature | Where-Object {$_.Name -eq "SNMP-Services"}
@@ -9,3 +10,8 @@ If ($check.Installed -ne "True") {
 }
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\ValidCommunities" /v $community_string /t REG_DWORD /d 4 /f | Out-Null
+
+ForEach ($manager in $permitted_managers) {
+  $i += 1
+  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers" /v $i /t REG_SZ /d $manager /f | Out-Null
+}
