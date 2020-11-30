2acbb95a154514c234eab7e50e522010d946e2b8
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-11-30 16:33:20 +0000

    Initial script

---
+++ investigate_disk_issues.ps1
@@ -0,0 +1,18 @@
+.<#
+.SYNOPSIS
+  Perform analysis of disk errors and performance
+
+.DESCRIPTION
+  Check System event log for storage subsystem errors and group by month.  Looks for the following:
+    Event:129 - Request timed out (storport driver)
+    Event:153 - Request timed out (miniport driver)
+.OUTPUTS
+
+.EXAMPLE
+
+.LINK
+  https://askbob.tech/understanding-storage-timeouts-and-event-129-errors/
+  https://askbob.tech/interpreting-event-153-errors/
+#>
+
+Get-WinEvent –FilterHashtable @{logname='System'; id=129,153} | Group-Object {$_.TimeCreated.ToString("yyyy-MM")} | Select-Object Name, Count
