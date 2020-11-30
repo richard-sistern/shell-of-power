6a70ddb95e69d0cc4185170a528c792f769742ec
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-11-30 16:02:25 +0000

    Initial script

---
+++ dhcp_snippets.ps1
@@ -0,0 +1,37 @@
+.<#
+.SYNOPSIS
+  Snippets for managing DHCP servers
+
+.DESCRIPTION
+
+
+.OUTPUTS
+
+.EXAMPLE
+
+.LINK
+
+#>
+
+# Set the DHCP server FQDN
+[string] $dhcp_server = "dhcp.server.fqdn"
+
+# List existing scopes
+Get-DhcpServerv4Scope -ComputerName $dhcp_server
+
+# List server level options
+Get-DhcpServerv4OptionValue -ComputerName $dhcp_server
+
+# Show scope statistics
+Get-DhcpServerv4ScopeStatistics -ComputerName $dhcp_server
+
+# List options for each scope
+$scopes = Get-DhcpServerv4Scope -ComputerName $dhcp_server
+Foreach ($scope in $scopes)
+{
+  "$($scope.Name) ($($scope.ScopeId))"
+  Get-DhcpServerv4OptionValue -ComputerName $dhcp_server -Scope $scope.ScopeId
+}
+
+# Remove a specific option from a scope
+Remove-DhcpServerv4OptionValue -ComputerName $dhcp_server -ScopeId 10.10.10.0 -OptionId 44
