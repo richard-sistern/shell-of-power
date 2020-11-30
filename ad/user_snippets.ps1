1d70811a7b1e3cf2f1c317f7840b4166a0c9bb92
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-11-30 15:45:15 +0000

    Formatting

--- ad/user_snippets.ps1
+++ ad/user_snippets.ps1
@@ -14,6 +14,6 @@
 
 
 # Simple export of active mailboxes
-Get-Aduser -Properties mail, description, office -filter {enabled -eq $true -and mail -like '*'} |
+Get-Aduser -Properties mail, description, office -Filter {enabled -eq $true -and mail -like '*'} |
     Select-Object GivenName, Surname, Mail, Description, Office |
         Export-Csv c:\temp\ad_mailboxes.csv
