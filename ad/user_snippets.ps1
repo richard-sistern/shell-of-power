3ab68c35bbfec535dc6534df51e818e38dde73dd
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2020-12-11 10:17:45 +0000

    Added group membership

--- ad/user_snippets.ps1
+++ ad/user_snippets.ps1
@@ -17,3 +17,13 @@
 Get-Aduser -Properties mail, description, office -Filter {enabled -eq $true -and mail -like '*'} |
     Select-Object GivenName, Surname, Mail, Description, Office |
         Export-Csv c:\ad_mailboxes.csv
+
+# Add user to a group
+$group = Get-ADGroup "Name of group"
+$user = Get-ADUser -filter { UserPrincipalName -eq "Name of user" }
+Add-ADGroupMember $group -Members $user
+
+# Note: Add-ADGroupMember does not accept pipeline input
+
+
+
