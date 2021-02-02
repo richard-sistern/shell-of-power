5b8f7415ad9856a19e48d4e6f12f812e81af762b
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-02-02 20:22:16 +0000

    Added security snippets

---
+++ ps/security.ps1
@@ -0,0 +1,16 @@
+.<#
+.SYNOPSIS
+  Snippets for managing security in PowerShell
+
+.DESCRIPTION
+
+
+.OUTPUTS
+
+.EXAMPLE
+
+.LINK
+#>
+
+# Allow unsigned scripts to run in the current process
+Set-ExecutionPolicy Bypass -Scope Process
