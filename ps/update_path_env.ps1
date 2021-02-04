3fdcf1ba7ab2c8f59388c2355c679758b952c3d5
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-02-04 15:05:34 +0000

    Update update_path_env.ps1

--- ps/update_path_env.ps1
+++ ps/update_path_env.ps1
@@ -3,5 +3,9 @@
   Add something to the path environment variable
 #>
 
+# Show current PATH
+[Environment]::GetEnvironmentVariable('Path', 'Machine') 
+
+# Update PATH
 $path = [Environment]::GetEnvironmentVariable('Path', 'Machine') + ';C:\folder'
 [Environment]::SetEnvironmentVariable("Path", $path, 'Machine')
