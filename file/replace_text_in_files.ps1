c098f3f96fa28304ad615cae1ca135ac379eae1c
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-02-09 09:38:46 +0000

    Add in place edit warning

--- file/replace_text_in_files.ps1
+++ file/replace_text_in_files.ps1
@@ -2,4 +2,5 @@
 Get-ChildItem 'c:\temp' -Filter *.txt | Get-Content | Select-String -Pattern 'FINDME'
 
 # Replace text in multiple files
+# In place editing, use with caution!
 Get-ChildItem 'c:\temp' -Filter *.txt  | ForEach { (Get-Content $_ | ForEach {$_ -replace "old", "new"}) | Set-Content $_ }
