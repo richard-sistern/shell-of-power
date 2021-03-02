8b1f4748c72d34a8b8d0c29afa43ca8ca106c11e
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-03-02 15:20:53 +0000

    Update computer_snippets.ps1

--- ad/computer_snippets.ps1
+++ ad/computer_snippets.ps1
@@ -16,5 +16,7 @@
 Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -Like '*Windows Server*' } -Properties OperatingSystem | Select OperatingSystem | Group-Object -Property OperatingSystem | Select Name, Count | Sort Name
 
 # List desktops with IP and last logon
-Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -notlike '*Windows Server*' } -Properties Name, Operatingsystem, OperatingSystemVersion, IPv4Address, LastLogonTimeStamp |
+$ComputerProperties = 'Name', 'Operatingsystem', 'OperatingSystemVersion', 'IPv4Address', 'LastLogonTimeStamp'
+
+Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -notlike '*Windows Server*' } -Properties $ComputerProperties  |
     Select Name, Operatingsystem, OperatingSystemVersion, IPv4Address, @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogonTimeStamp)}}
