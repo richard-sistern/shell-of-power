73516ff18b25e4914206b5ca3fa002d19226d900
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-03-02 12:14:51 +0000

    add powershell to retrieve list of desktops from AD

--- ad/computer_snippets.ps1
+++ ad/computer_snippets.ps1
@@ -12,5 +12,9 @@
 
 #>
 
-# Show count of Windows server OS
+# Show count of Windows Server OS
 Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -Like '*Windows Server*' } -Properties OperatingSystem | Select OperatingSystem | Group-Object -Property OperatingSystem | Select Name, Count | Sort Name
+
+# List desktops with IP and last logon
+Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -notlike '*Windows Server*' } -Properties Name, Operatingsystem, OperatingSystemVersion, IPv4Address, LastLogonTimeStamp |
+    Select Name, Operatingsystem, OperatingSystemVersion, IPv4Address, @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogonTimeStamp)}}
