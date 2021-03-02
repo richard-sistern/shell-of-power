941b69474d053dccae4948e860f026cc71131d28
Richard Sistern <richard.sistern@nnuh.nhs.uk>
2021-03-02 15:22:25 +0000

    fix ad search for desktops

--- ad/computer_snippets.ps1
+++ ad/computer_snippets.ps1
@@ -16,7 +16,5 @@
 Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -Like '*Windows Server*' } -Properties OperatingSystem | Select OperatingSystem | Group-Object -Property OperatingSystem | Select Name, Count | Sort Name
 
 # List desktops with IP and last logon
-$ComputerProperties = 'Name', 'Operatingsystem', 'OperatingSystemVersion', 'IPv4Address', 'LastLogonTimeStamp'
-
-Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -notlike '*Windows Server*' } -Properties $ComputerProperties  |
+Get-ADComputer -Filter { Enabled -eq $True -and OperatingSystem -notlike '*Windows Server*' } -Properties Name, Operatingsystem, OperatingSystemVersion, IPv4Address, LastLogonTimeStamp |
     Select Name, Operatingsystem, OperatingSystemVersion, IPv4Address, @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogonTimeStamp)}}
