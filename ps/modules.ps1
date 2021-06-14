68908b738c667445c2b9385a11f73eec730f3caf
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-06-14 14:42:13 +0100

    Update modules.ps1

--- ps/modules.ps1
+++ ps/modules.ps1
@@ -5,3 +5,12 @@
 
 # Where is a module installed
 (Get-Module -ListAvailable ActiveDirectory*).path
+
+# To resolve the following error when running Install-Module -name <name>
+# WARNING: Unable to resolve package source 'https://www.powershellgallery.com/api/v2'.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Sometimes have to use
+$webclient=New-Object System.Net.WebClient
+$webclient.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+[Net.ServicePointManager]::SecurityProtocol = "tls12"
