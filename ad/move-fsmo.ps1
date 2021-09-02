ef97ce58a057193dcefa0cfbc2d90e68561d394d
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-09-02 08:39:15 +0100

    Update move-fsmo.ps1

--- ad/move-fsmo.ps1
+++ ad/move-fsmo.ps1
@@ -1,1 +1,11 @@
+# Transfer all FSMO roles to a target machine
+
+# Roles:
+# 0	PDC Emulator
+# 1	RID Master
+# 2	Infrastructure Master
+# 3	Schema Master
+# 4	Domain Naming Master
+
+Move-ADDirectoryServerOperationMasterRole -Identity "Target DC" –OperationMasterRole 0,1,2,3,4 -Confirm:$false -Force
 
