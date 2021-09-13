0c6f7bd16e491204f112c4de27cc550f8e02e0e0
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-09-13 10:28:17 +0100

    Update Enable-RecycleBin.ps1

--- ad/Enable-RecycleBin.ps1
+++ ad/Enable-RecycleBin.ps1
@@ -1,1 +1,5 @@
+# Check current status
+Get-ADOptionalFeature "Recycle Bin Feature" | Select-Object name, enabledscopes
+
+# Enable recycle bin
 Enable-ADOptionalFeature "Recycle Bin Feature" -Scope ForestOrConfigurationSet -Target <forest root domain name>
